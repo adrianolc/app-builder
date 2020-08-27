3c555130ce196a3859f14a94f2fe528f7f34c55e
Adriano Cordeiro <adrianolcx@gmail.com>
2020-08-27 09:24:18 +0200

    Add github repo config

--- settings.py
+++ settings.py
@@ -6,3 +6,5 @@ load_dotenv()
 
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 GITHUB_URL = os.getenv('GITHUB_BASE_URL')
+GITHUB_REPO_URL = os.getenv('GITHUB_REPO_URL')
+GITHUB_REPO_NAME = os.getenv('GITHUB_REPO_NAME')
